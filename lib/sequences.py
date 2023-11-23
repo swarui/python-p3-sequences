d3da20075a1501a0afaed86064b035c054017915
swarui <stevewaruim@gmail.com>
2023-11-23 13:16:53 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    if length == 0:
+        print(fib)
+    elif length == 1:
+        fib.append(0)
+        print(fib)
+    elif length == 2:
+        fib.append(0)
+        fib.append(1)
+        print(fib)
+    else:
+        fib.append(0)
+        fib.append(1)
+        for i in range(3, length+1):
+            fib.append(fib[i-3] + fib[i-2])
+
+        print(fib)
+
+
